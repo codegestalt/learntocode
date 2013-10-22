abe3996935af520530df4d3b8797d61ff945f8f3
Rodrigo Haenggi <rodrigo@codegestalt.com>
2013-10-22 17:35:24 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+ruby '2.0.0'
+
+# Core
+gem 'rails', '4.0.0'
+gem 'rails_12factor'
+gem 'sass-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+
+# Frontend
+gem 'compass-rails', github: "Compass/compass-rails", branch: "rails4-hack"
+gem 'font-awesome-rails'
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kids::Application.config.secret_key_base = '3c07d9a6fad306147293de88cbb9c5fc9e71dea1a8f28213212ee3d92c1168a3f16fdbc9ee223601b6c96bda9a9813b5efde7dbf593258632db1eed20434f2e7'
